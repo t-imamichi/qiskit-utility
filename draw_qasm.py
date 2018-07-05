cce38f545c0ca3095bba35164e6461076e205c6c
Takashi Imamichi <imamichi@jp.ibm.com>
2018-07-05 18:01:54 +0900

    initial import

---
+++ draw_qasm.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright 2017, IBM.
+#
+# This source code is licensed under the Apache License, Version 2.0 found in
+# the LICENSE.txt file in the root directory of this source tree.
+
+'''
+Draw a QASM circuit based on matplotlib_circuit_drawer
+'''
+
+import json
+from argparse import ArgumentParser
+
+import matplotlib
+
+matplotlib.use('module://ipykernel.pylab.backend_inline')  # workaround to avoid redundant draw path
+from qiskit.tools.visualization._circuit_visualization import MatplotlibDrawer
+
+
+def options():
+    parser = ArgumentParser()
+    parser.add_argument('--qasm', action='store', help='input QASM file')
+    parser.add_argument('--style', action='store', help='style file')
+    parser.add_argument('--scale', action='store', help='scaling factor', type=float, default=1.0)
+    parser.add_argument('--out', action='store', help='output figure file (pdf, png or svg)')
+    parser.add_argument('--json', action='store', help='output JSON file of AST')
+    parser.add_argument('--verbose', action='store_true', help='verbose')
+    args = parser.parse_args()
+    if args.verbose:
+        print('options:', args)
+    if not args.qasm:
+        parser.print_usage()
+    return args
+
+
+def main():
+    args = options()
+    if not args.qasm:
+        return
+    drawer = MatplotlibDrawer(style=args.style, scale=args.scale)
+    drawer.load_qasm_file(args.qasm)
+    # output json
+    if args.json:
+        with open(args.json, 'w') as outfile:
+            json.dump(drawer.ast, outfile, sort_keys=True, indent=2)
+    # draw quantum circuit
+    drawer.draw(args.out)
+
+
+if __name__ == '__main__':
+    main()
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Utility scripts for QISKit
+
+- run_qasm.py
+  Run a qasm and check the result.
+
+- draw_qasm.py
+  Draw a circuit to pdf, svg or png file.
+
